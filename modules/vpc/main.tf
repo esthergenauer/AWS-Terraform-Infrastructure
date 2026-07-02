a426ea8580a2a3073e6ebc5340fc6a406e072a65
Esther Genauer <esthergenauer@gmail.com>
2026-07-02 22:04:59 +0300

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  tags = { Name = "${var.project_name}-private-subnet" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  tags = { Name = "${var.project_name}-public-subnet" }
+}
